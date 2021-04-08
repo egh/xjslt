41d3fbde1f786e3e3847aa866c722d22cc08773d
Erik Hetzner <egh@e6h.org>
2021-04-07 17:21:22 -0700

    Format fix

--- src/cli.ts
+++ src/cli.ts
@@ -6,10 +6,12 @@ import { existsSync, readFileSync, writeFileSync } from "fs";
 import { sync } from "slimdom-sax-parser";
 const program = new Command();
 program.version("0.0.1");
-program.arguments("<xslt> <xml...>").description("Transform XML", {
-  xslt: "XSLT stylesheet",
-  xml: "XML files to process",
-})
+program
+  .arguments("<xslt> <xml...>")
+  .description("Transform XML", {
+    xslt: "XSLT stylesheet",
+    xml: "XML files to process",
+  })
   .action((xslt, xmls) => {
     const transform = buildStylesheet(xslt);
     const serializer = new slimdom.XMLSerializer();
@@ -19,9 +21,8 @@ program.arguments("<xslt> <xml...>").description("Transform XML", {
         throw new Error("File " + outfile + " exists!");
       } else {
         const xmlDom = sync(readFileSync(xml).toString());
-        const transformed = transform(sync(readFileSync(xml).toString()));
-        writeFileSync(outfile,
-                      serializer.serializeToString(transformed));
+        const transformed = transform(xmlDom);
+        writeFileSync(outfile, serializer.serializeToString(transformed));
       }
     }
   });
