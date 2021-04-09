9f6db0d931f3e4878e1590257d08382a5ac82cf0
Erik Hetzner <egh@e6h.org>
2021-04-09 11:36:55 -0700

    Rewrite cli to write directly to stdout, minimizing xsltproc

--- src/cli.ts
+++ src/cli.ts
@@ -2,29 +2,29 @@
 import * as slimdom from "slimdom";
 import { Command } from "commander";
 import { buildStylesheet } from "./xjslt";
-import { existsSync, readFileSync, writeFileSync } from "fs";
-import { sync } from "slimdom-sax-parser";
-const program = new Command();
-program.version("0.0.1");
-program
-  .arguments("<xslt> <xml...>")
-  .description("Transform XML", {
-    xslt: "XSLT stylesheet",
-    xml: "XML files to process",
-  })
-  .action((xslt, xmls) => {
-    const transform = buildStylesheet(xslt);
-    const serializer = new slimdom.XMLSerializer();
-    for (let xml of xmls) {
-      const outfile = xml + "_out";
-      if (existsSync(outfile)) {
-        throw new Error("File " + outfile + " exists!");
-      } else {
-        const xmlDom = sync(readFileSync(xml).toString());
-        const transformed = transform(xmlDom);
-        writeFileSync(outfile, serializer.serializeToString(transformed));
-      }
-    }
-  });
+import { createReadStream } from "fs";
+import { async } from "slimdom-sax-parser";
 
-program.parse(process.argv);
+async function run(xslt: string, xmls: Array<string>) {
+  const transform = buildStylesheet(xslt);
+  const serializer = new slimdom.XMLSerializer();
+  for (let xml of xmls) {
+    const xmlDom = await async(createReadStream(xml));
+    const transformed = transform(xmlDom);
+    process.stdout.write(serializer.serializeToString(transformed));
+  }
+}
+
+async function main() {
+  const program = new Command();
+  program.version("0.0.1");
+  program
+    .arguments("<xslt> <xml...>")
+    .description("Transform XML", {
+      xslt: "XSLT stylesheet",
+      xml: "XML files to process",
+    })
+    .action(run);
+  await program.parseAsync(process.argv);
+}
+main();
