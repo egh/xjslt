b203f23c81521df00d45e522fdaec79817a04296
Erik Hetzner <egh@e6h.org>
2021-04-09 11:55:08 -0700

    Add LGPL license

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,24 @@
 #!/usr/bin/env node
+/*
+ * Copyright (C) 2021 Erik Hetzner
+ *
+ * This file is part of XJSLT.
+ *
+ * XJSLT is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * XJSLT is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with XJSLT. If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
+
 import * as slimdom from "slimdom";
 import { Command } from "commander";
 import { buildStylesheet } from "./xjslt";
