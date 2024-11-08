c6f92498770104cb517eb6b96814e7542908f699
Martin Middel <martin@elliat.nl>
2024-11-08 10:23:24 +0100

    Optimize performance of pattern match cache
    A profile of the JATS example showed a 30% overall time in the `evaluateXPathToString` function,
    which was almost all in generate-id. Which is used to construct a cache key for nodes and
    patterns. Replacing the use of generate-id with a nested map for the cache makes it disappear from
    the profile!

--- src/definitions.ts
+++ src/definitions.ts
@@ -180,7 +180,7 @@ export interface DynamicContext {
   currentGroup?: any[];
   currentGroupingKey?: string;
   keys: Map<String, Key>;
-  patternMatchCache: Map<string, Set<slimdom.Node>>;
+  patternMatchCache: PatternMatchCache;
   outputDefinitions: Map<string, OutputDefinition>;
   stylesheetParams?: object;
 }
@@ -205,18 +205,20 @@ export interface xpathstring {
 
 export type AttributeValueTemplate = Array<string | xpathstring>;
 
+export type PatternMatchCache =  Map<string, Map<slimdom.Node, Set<slimdom.Node>>>;
+
 export interface Key {
   match: string;
   use: string | SequenceConstructor;
   namespaces: object;
   cache: Map<slimdom.Document, Map<any, any>>;
   buildDocumentCache: (
-    patternMatchCache: Map<string, Set<slimdom.Node>>,
+    patternMatchCache: PatternMatchCache,
     document: slimdom.Document,
     variableScopes: VariableScope[],
   ) => Map<any, any>;
   lookup: (
-    patternMatchCache: Map<string, Set<slimdom.Node>>,
+    patternMatchCache: PatternMatchCache,
     document: slimdom.Document,
     variableScopes: VariableScope[],
     value: any,
